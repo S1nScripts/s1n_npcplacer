c3db14ab9027f8ed7bb6f73c4e2853e5d088c368
Sinyx <sinyx.contact@gmail.com>
2023-01-29 16:04:47 +0100

    modify: added OOP on main for NPC

--- client/main.lua
+++ client/main.lua
@@ -1,61 +1,59 @@
--- Create a NPC
--- @param {string} model - Model of the NPC
--- @param {vector4} position - Position of the NPC
-local function createNpc(model, position)
-    local model = GetHashKey(model)
+local MAX_TIMECOUNT = 100
 
-    RequestModel(model)
+local NPC = {}
+NPC.__index = NPC
+
+function NPC:new(model, position)
+    local object = {}
+    setmetatable(object, NPC)
+
+    object.model = GetHashKey(model)
+    object.ped = nil
+    object.position = position
+
+    RequestModel(object.model)
 
     local timeoutCount = 0
 
-    while not HasModelLoaded(model) and timeoutCount < 100 do
+    while not HasModelLoaded(object.model) and timeoutCount < 100 do
         timeoutCount = timeoutCount + 1
 
         Citizen.Wait(100)
     end
 
-    local ped = CreatePed(4, model, position.x, position.y, position.z - 1, position.w, false, true)
+    object.ped = CreatePed(4, object.model, position.x, position.y, position.z - 1, position.w, false, true)
+    FreezeEntityPosition(object.ped, true)
+    SetEntityInvincible(object.ped, true)
+    SetBlockingOfNonTemporaryEvents(object.ped, true)
 
-    FreezeEntityPosition(ped, true)
-    SetEntityInvincible(ped, true)
-    SetBlockingOfNonTemporaryEvents(ped, true)
-
-    return ped
+    return object
 end
 
--- Attach a prop to an NPC
--- @param {Ped} ped - The ped to attach the prop to
--- @param {Vector3} position - The position of the NPC
--- @param {table} propData - The data of the prop to attach
-local function attachPropToNpc(ped, position, propData)
+function NPC:attachProp(propData)
     local propHash = GetHashKey(propData.model)
 
     RequestModel(propHash)
 
     local timeoutCount = 0
 
-    while not HasModelLoaded(propHash) and timeoutCount < 100 do
+    while not HasModelLoaded(propHash) and timeoutCount < MAX_TIMECOUNT do
         timeoutCount = timeoutCount + 1
 
         Citizen.Wait(100)
     end
 
-    local object = CreateObject(propHash, position.x, position.y, position.z, true, true, true)
-    AttachEntityToEntity(object, ped, GetPedBoneIndex(ped, propData.bone and propData.bone or 60309), propData.position.x, propData.position.y, propData.position.z, propData.rotation.x, propData.rotation.y, propData.rotation.z, true, true, false, true, 0, true)
+    local object = CreateObject(propHash, self.position.x, self.position.y, self.position.z, true, true, true)
+    AttachEntityToEntity(object, self.ped, GetPedBoneIndex(self.ped, propData.bone and propData.bone or 60309), propData.position.x, propData.position.y, propData.position.z, propData.rotation.x, propData.rotation.y, propData.rotation.z, true, true, false, true, 0, true)
 end
 
--- Set an animation to the NPC
--- @param {Ped} ped - The ped to play the animation
--- @param {string} dict - The dictionary to play the animation
--- @param {string} name - The name of the animation
-local function setNPCAnimation(ped, animationDict, animationName)
+function NPC:playAnimation(animationDict, animationName)
     RequestAnimDict(animationDict)
 
     while not HasAnimDictLoaded(animationDict) do
         Citizen.Wait(1000)
     end
 
-    TaskPlayAnim(ped, animationDict, animationName, 1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
+    TaskPlayAnim(self.ped, animationDict, animationName, 1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
 end
 
 -- Load a NPC with possibility to play an animation and attach a prop to it
@@ -63,23 +61,23 @@ end
 -- @param {table} position - Position of the NPC
 -- @param {table} animation - Animation data of the NPC
 -- @param {table} prop - Prop data to attach to the NPC
-local function loadNpc(model, position, animData, propData)
+local function loadNpc(npcData)
     Citizen.CreateThread(function()
-        local ped = createNpc(model, position)
+        local npc = NPC:new(npcData.model, npcData.position)
 
-        if propData.enable then
-            for _, propData in pairs(propData.list) do
-                attachPropToNpc(ped, position, propData)
+        if npcData.props.enable then
+            for _, propData in pairs(npcData.props.list) do
+                npc:attachProp(propData)
             end
         end
 
-        if animData.enable then
-            setNPCAnimation(ped, animData.dict, animData.name)
+        if npcData.animation.enable then
+            npc:playAnimation(npcData.animation.dict, npcData.animation.name)
         end
     end)
 end
 
 -- Load all npcs from the config file
 for _, npcData in pairs(Config.npcs) do
-    loadNpc(npcData.model, npcData.position, npcData.animation, npcData.props)
+    loadNpc(npcData)
 end
